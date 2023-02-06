f244233b19130e8cba844be0f4ace4b018b3e770
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2023-02-06 18:16:35 +0800

    Fix for ColorOS 13 in 1+11 (#4)

--- customize.sh
+++ customize.sh
@@ -3,8 +3,9 @@ SYSTEM_PATH=/system
 SYSTEM_EXT_PATH=$SYSTEM_PATH/system_ext
 PRODUCT_PATH=$SYSTEM_PATH/product
 VENDOR_PATH=$SYSTEM_PATH/vendor
-OPLUS_BIGBALL_PATH=/mnt/vendor/my_bigball
-ROOT_LIST=""$SYSTEM_PATH$PERMISSIONS_PATH" "$PRODUCT_PATH$PERMISSIONS_PATH" "$VENDOR_PATH$PERMISSIONS_PATH" "$SYSTEM_EXT_PATH$PERMISSIONS_PATH" "$OPLUS_BIGBALL_PATH$PERMISSIONS_PATH""
+OPLUS_BIGBALL_PATH=/my_bigball
+OPLUS_BIGBALL_VENDOR_PATH=/mnt/vendor$OPLUS_BIGBALL_PATH
+ROOT_LIST=""$SYSTEM_PATH$PERMISSIONS_PATH" "$PRODUCT_PATH$PERMISSIONS_PATH" "$VENDOR_PATH$PERMISSIONS_PATH" "$SYSTEM_EXT_PATH$PERMISSIONS_PATH" "$OPLUS_BIGBALL_PATH$PERMISSIONS_PATH" "$OPLUS_BIGBALL_VENDOR_PATH$PERMISSIONS_PATH""
 FILE_LIST="services.cn.google.xml cn.google.services.xml oplus_google_cn_gms_features.xml"
 for ROOT in $ROOT_LIST; do
     for FILE in $FILE_LIST; do
@@ -12,23 +13,22 @@ for ROOT in $ROOT_LIST; do
             PERMISSION_PATH="$MODPATH$ROOT"
             FILE_NAME=$FILE
             ui_print "- PATH $ROOT/$FILE_NAME"
-            break 2
+            mkdir -p "$PERMISSION_PATH"
+            cat >"$PERMISSION_PATH/$FILE_NAME" <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<!-- This is the standard set of features for devices that support the CN GMSCore. -->
+EOF
+            [ "$ROOT" = "$OPLUS_BIGBALL_PATH$PERMISSIONS_PATH" ] || [ "$ROOT" = "$OPLUS_BIGBALL_VENDOR_PATH$PERMISSIONS_PATH" ]  && {
+                if [ ! -f "$MODPATH/post-fs-data.sh" ]; then
+                    cat >"$MODPATH/post-fs-data.sh" <<EOF
+#!/system/bin/sh
+MODDIR=\${0%/*}
+EOF
+                fi
+                echo "mount -o ro,bind \$MODDIR$ROOT/$FILE_NAME $ROOT/$FILE_NAME" >> "$MODPATH/post-fs-data.sh"
+            }
         fi
     done
 done
 
 [ -n "$PERMISSION_PATH" ] || abort "$FILE_LIST Not found!"
-mkdir -p "$PERMISSION_PATH"
-
-cat >"$PERMISSION_PATH/$FILE_NAME" <<EOF
-<?xml version="1.0" encoding="utf-8"?>
-<!-- This is the standard set of features for devices that support the CN GMSCore. -->
-EOF
-
-[ "$PERMISSION_PATH" = "$MODPATH$OPLUS_BIGBALL_PATH$PERMISSIONS_PATH" ] && {
-    cat >"$MODPATH/post-fs-data.sh" <<EOF
-#!/system/bin/sh
-MODDIR=\${0%/*}
-mount -o ro,bind \$MODDIR$OPLUS_BIGBALL_PATH$PERMISSIONS_PATH/$FILE_NAME $OPLUS_BIGBALL_PATH$PERMISSIONS_PATH/$FILE_NAME
-EOF
-}
